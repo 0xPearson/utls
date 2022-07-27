9887ceb17a5a421573ad42162a851120a45cebfa
Complacing <devantepearson@hotmail.com>
2022-07-27 04:30:07 -0400

    g

--- u_common.go
+++ u_common.go
@@ -144,13 +144,14 @@ var (
 	HelloFirefox_63   = ClientHelloID{helloFirefox, "63", nil}
 	HelloFirefox_65   = ClientHelloID{helloFirefox, "65", nil}
 
-	HelloChrome_Auto = HelloChrome_100
+	HelloChrome_Auto = HelloChrome_103
 	HelloChrome_58   = ClientHelloID{helloChrome, "58", nil}
 	HelloChrome_62   = ClientHelloID{helloChrome, "62", nil}
 	HelloChrome_70   = ClientHelloID{helloChrome, "70", nil}
 	HelloChrome_72   = ClientHelloID{helloChrome, "72", nil}
 	HelloChrome_83   = ClientHelloID{helloChrome, "83", nil}
 	HelloChrome_100  = ClientHelloID{helloChrome, "100", nil}
+	HelloChrome_103  = ClientHelloID{helloChrome, "103", nil}
 
 	HelloIOS_Auto = HelloIOS_12_1
 	HelloIOS_11_1 = ClientHelloID{helloIOS, "111", nil} // legacy "111" means 11.1
